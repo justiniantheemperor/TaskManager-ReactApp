9db4ad7e2e66f0272f2945a6cd84814b22f0a7a1
justiniantheemperor <justinrellis52@gmail.com>
2023-04-21 13:38:36 -0600

    update filler data

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ const tasksData = [
     completed: 'true',
     name: 'Do Dishes',
     description: 'Rinse off all the dishes and put them in the washing machine.',
-    date: '2019-04-11',
+    hours: 1,
     dueDate: '2019-04-11',
   },
   {
@@ -12,7 +12,7 @@ const tasksData = [
     completed: 'false',
     name: 'Take Out the Trash',
     description: 'Make sure you replace the liner after taking out the trash!',
-    date: '2019-04-17',
+    hours: .5,
     dueDate: '2019-04-17',
   },
 ];
